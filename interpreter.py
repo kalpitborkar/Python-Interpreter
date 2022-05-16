7e67f0226c70196c6d82ccf936a345846776d001
kalpitborkar <81480133+kalpitborkar@users.noreply.github.com>
2022-05-16 17:02:04 +0530

    Complete basic interpreter

--- interpreter.py
+++ interpreter.py
@@ -1,17 +1,24 @@
 
+from socket import if_nametoindex
+
+
 what_to_execute = {
     "instructions": [("LOAD_VALUE", 0),
+                     ("STORE_NAME", 0),
                      ("LOAD_VALUE", 1),
-                     ("ADD_TWO_VALUES", None),
-                     ("LOAD_VALUE", 2),
+                     ("STORE_NAME", 1),
+                     ("LOAD_NAME", 0),
+                     ("LOAD_NAME", 1),
                      ("ADD_TWO_VALUES", None),
                      ("PRINT_ANSWER", None)],
-    "numbers": [7, 5, 8]}
+    "numbers": [1, 2],
+    "names":   ["a", "b"]}
 
 
 class Interpreter:
     def __init__(self):
         self.stack = []
+        self.environment = {}
 
     def LOAD_VALUE(self, number):
         self.stack.append(number)
@@ -26,18 +33,50 @@ class Interpreter:
         total = first_num + second_num
         self.stack.append(total)
 
+    def STORE_NAME(self, name):
+        val = self.stack.pop()
+        self.environment[name] = val
+
+    def LOAD_NAME(self, name):
+        val = self.environment[name]
+        self.stack.append(val)
+
+    def parse_argument(self, instruction, argument, what_to_execute):
+        numbers = ["LOAD_VALUE"]
+        names = ["LOAD_NAME", "STORE_NAME"]
+        if instruction in numbers:
+            argument = what_to_execute["numbers"][argument]
+        elif instruction in names:
+            argument = what_to_execute["names"][argument]
+
     def run_code(self, what_to_execute):
         instructions = what_to_execute["instructions"]
-        numbers = what_to_execute["numbers"]
         for each_step in instructions:
             instruction, argument = each_step
+            argument = self.parse_argument(
+                instruction, argument, what_to_execute)
+
             if instruction == "LOAD_VALUE":
-                number = numbers[argument]
-                self.LOAD_VALUE(number)
+                self.LOAD_VALUE(argument)
             elif instruction == "ADD_TWO_VALUES":
                 self.ADD_TWO_VALUES()
             elif instruction == "PRINT_ANSWER":
                 self.PRINT_ANSWER()
+            elif instruction == "STORE_NAME":
+                self.STORE_NAME(argument)
+            elif instruction == "LOAD_NAME":
+                self.LOAD_NAME(argument)
+                
+    def execute(self, what_to_execute):
+        instructions = what_to_execute["instructions"]
+        for each_step in instructions:
+            instruction, argument = each_step
+            argument = self.parse_argument(instruction, argument, what_to_execute)
+            bytecode_method = getattr(self, instruction)
+            if argument is None:
+                bytecode_method()
+            else:
+                bytecode_method(argument)
 
 
 interpreter = Interpreter()
