d0e5f5628a8f4b9abce70ccdf4ab4cc31a1d9d28
kalpitborkar <81480133+kalpitborkar@users.noreply.github.com>
2022-05-17 10:04:50 +0530

    Add frame manipulation

--- byterun.py
+++ byterun.py
@@ -18,6 +18,37 @@ class VirtualMachine(object):
             code, global_names=global_names, local_names=local_names)
         self.run_frame(frame)
 
+    def make_frame(self, code, callargs={}, global_names=None, local_names=None):
+        if global_names is not None and local_names is not None:
+            local_names = global_names
+        elif self.frames:
+            global_names = self.frame.global_names
+            local_names = {}
+        else:
+            global_names = local_names = {
+                '__builtins': __builtins__,
+                '__name__': '__main__',
+                '__doc__': None,
+                '__package__': None
+            }
+        local_names.update(callargs)
+        frame = Frame(code, global_names, local_names, self.frame)
+        return frame
+    
+    def push_frame(self, frame):
+        self.frames.append(frame)
+        self.frame = frame
+    
+    def pop_frame(self):
+        self.frames.pop()
+        if self.frames:
+            self.frame = self.frames[-1]
+        else:
+            self.frame = None
+            
+    def run_frame(self):
+        pass
+
 
 class Frame(object):
     def __init__(self, code_obj, global_names, local_names, prev_frame):
