8bfea0d1f345346d7505657c67888fb1970ab204
kalpitborkar <81480133+kalpitborkar@users.noreply.github.com>
2022-05-17 12:09:58 +0530

    Update byterun and rename interpreter

--- byterun.py
+++ byterun.py
@@ -12,6 +12,8 @@ class VirtualMachine(object):
         self.frame = None
         self.return_value = None
         self.last_exception = None
+        
+
 
     def run_code(self, code, global_names=None, local_names=None):
         frame = self.make_frame(
@@ -34,20 +36,44 @@ class VirtualMachine(object):
         local_names.update(callargs)
         frame = Frame(code, global_names, local_names, self.frame)
         return frame
-    
+
     def push_frame(self, frame):
         self.frames.append(frame)
         self.frame = frame
-    
+
     def pop_frame(self):
         self.frames.pop()
         if self.frames:
             self.frame = self.frames[-1]
         else:
             self.frame = None
-            
+
     def run_frame(self):
         pass
+    
+    def top(self):
+        return self.frame.stack[-1]
+
+    def pop(self):
+        return self.frame.stack.pop()
+
+    def push(self, *vals):
+        self.frame.stack.exetend(vals)
+
+    def popn(self, n):
+        if n:
+            ret = self.frame.stack[-n:]
+            self.frame.stack[-n:] = []
+            return ret
+        else:
+            return []
+        
+    def parse_byte_and_args(self):
+        f = self.frame
+        opoffset = f.last_instruction
+        byteCode = f.code_obj.co_code[opoffset]
+        f.last_instruction += 1
+        byte_name = dis.opname[byteCode]
 
 
 class Frame(object):
@@ -65,3 +91,50 @@ class Frame(object):
                 self.builtin_names = self.builtin_names.__dict__
         self.last_instruction = 0
         self.block_stack = []
+
+
+class Function(object):
+    """
+    Create a realistic function object, defining the things the interpreter expects.
+    """
+    __slots__ = [
+        'func_code', 'func_name', 'func_defaults', 'func_globals',
+        'func_locals', 'func_dict', 'func_closure',
+        '__name__', '__dict__', '__doc__',
+        '_vm', '_func',
+    ]
+
+    def __init__(self, name, code, globs, defaults, closure, vm):
+        self._vm = vm
+        self.func_code = code
+        self.func_name = self.__name__ = name or code.co_name
+        self.func_defaults = tuple(defaults)
+        self.func_globals = globs
+        self.func_locals = self._vm.frame.f_locals
+        self.__dict__ = {}
+        self.func_closure = closure
+        self.__doc__ = code.co_consts[0] if code.co_consts else None
+
+        # Sometimes, we need a real Python function.  This is for that.
+        kw = {
+            'argdefs': self.func_defaults,
+        }
+        if closure:
+            kw['closure'] = tuple(make_cell(0) for _ in closure)
+        self._func = types.FunctionType(code, globs, **kw)
+
+    def __call__(self, *args, **kwargs):
+        """When calling a Function, make a new frame and run it."""
+        callargs = inspect.getcallargs(self._func, *args, **kwargs)
+        # Use callargs to provide a mapping of arguments: values to pass into the new
+        # frame.
+        frame = self._vm.make_frame(
+            self.func_code, callargs, self.func_globals, {}
+        )
+        return self._vm.run_frame(frame)
+
+    def make_cell(value):
+        """Create a real Python closure and grab a cell."""
+        # Thanks to Alex Gaynor for help with this bit of twistiness.
+        fn = (lambda x: lambda: x)(value)
+        return fn.__closure__[0]
